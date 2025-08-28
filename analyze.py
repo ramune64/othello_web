1c73076e66f18bc1e5decd744a92325ae1ac5509
ramune64 <ramune69atrrsc@gmail.com>
2025-08-29 01:56:29 +0900

    いろいろ更新

--- analyze.py
+++ analyze.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("csvs/diff_stone.csv")
 
 # プレイヤーごとに色を変えて描画
-plt.figure(figsize=(12, 6))
+""" plt.figure(figsize=(12, 6))
 for i in range(9):
     if i==7:
         #sns.kdeplot(df[str(i)], label=f"Origin",fill=True)
@@ -16,31 +16,31 @@ for i in range(9):
         pass
     elif i!=7:
         sns.kdeplot(df[str(i)]-df[str(7)], label=f"Player {i}")
-        #sns.histplot(df[str(i)]-df[str(7)], bins=30, kde=False,label=f"Player {i}")
+        #sns.histplot(df[str(i)]-df[str(7)], bins=30, kde=False,label=f"Player {i}") """
 
-plt.title("Stone Difference Distribution (by Player)")
+""" plt.title("Stone Difference Distribution (by Player)")
 plt.xlabel("Stone Difference")
 plt.ylabel("Density")
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
-plt.show()
+plt.show() """
 
 #df = pd.read_csv("csvs/diff_stone.csv")
-for i in range(9):
+""" for i in range(9):
     #stat, p = stats.shapiro(df[str(i)])  # "0" はプレイヤー番号
     
     stat, p = stats.kstest(df[str(i)]-df[str(7)], "norm")
 
-    print(f"Shapiro-Wilk Test stat={stat}, p={p}")
+    print(f"Shapiro-Wilk Test stat={stat}, p={p}") """
 
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
-stats.probplot(df["0"]-df[str(7)], dist="norm", plot=plt)
+""" stats.probplot(df["0"]-df[str(7)], dist="norm", plot=plt)
 plt.title("Q-Q plot (Player 0)")
 plt.grid(True)
-plt.show()
+plt.show() """
 
 import  math
 import numpy as np
@@ -56,8 +56,8 @@ for i in range(9):
     t_stat, p_val = stats.ttest_ind(player, origin,equal_var=False)
     print(f"Player {i} vs Origin: t={t_stat}, p={p_val}")
 
-    # Player i vs Origin（例: i = 0）
+    """ # Player i vs Origin（例: i = 0）
     mean_diff = np.mean(player) - np.mean(df["7"])
     pooled_std = np.sqrt((np.var(player, ddof=1) + np.var(df["7"], ddof=1)) / 2)
     cohen_d = mean_diff / pooled_std
-    print(cohen_d)
+    print(cohen_d) """
---
+++ make_graph.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import scipy.stats as stats
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# CSV読み込み
+df = pd.read_csv("csvs/top_individuals.csv")
+
+
+for i in range(7,25):
+    print(i)
+    fitness_col = df.columns[i]
+
+    top_fitness = df[fitness_col].iloc[2::5].reset_index(drop=True)
+
+    rolling_mean = top_fitness.rolling(window=100).mean()
+
+    # プロット
+    plt.figure(figsize=(12,6))
+    plt.plot(top_fitness, label=f"{fitness_col} (raw)")   # 薄く描画
+    plt.plot(rolling_mean, label="100-gen moving average", color="red", linewidth=2,alpha = 0.6)
+    plt.xlabel("Generation")
+    plt.ylabel("Fitness")
+    plt.legend()
+    plt.savefig(f"fig_{fitness_col}.png", dpi=300, bbox_inches="tight")
+    #plt.show()
